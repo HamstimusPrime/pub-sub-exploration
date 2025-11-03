bc762203980ef75212255d2570d5581a0c037413
Mohammed Dadi <artofmohammed@gmail.com>
2025-11-04 00:31:05 +0100

    feat: implement RabbitMQ pub/sub foundation with Docker setup
    - Add Dockerfile for RabbitMQ server with STOMP plugin
    - Add rabbitmq-amqp091-go dependency to go.mod
    - Create pubsub package with generic PublishJSON function
    - Create routing package with constants and models for game state
    - Implement server main.go with RabbitMQ connection and pause message publishing
    - Add signal handling for graceful shutdown

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	pubsub "github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	routing "github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+	
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	conctString := "amqp://guest:guest@localhost:5672/"
+	connection, err := amqp.Dial(conctString)
+	if err != nil {
+		fmt.Println("unable to establish connection to server...")
+	}
+	defer connection.Close()
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan //this is a technique to block the execution of the code below until a value has been passed into signalChan
+	fmt.Println("exiting program...")
+
+	channel,err := connection.Channel()
+	if err != nil {
+		fmt.Println("unable to establish create connection channel...")
+		return
+	}
+	err = pubsub.PublishJSON(channel,
+		routing.ExchangePerilDirect, 
+		routing.PauseKey,
+		routing.PlayingState{
+			IsPaused: true,
+		},
+	)
+	if err != nil {
+		fmt.Println("unable to pusblish message")
+		return
+	}
+
+
 }
